1e30146ad8f2dd5d2dd865ba8b8299aa157d41e3
Aadil2011 <84117591+Aadil2011@users.noreply.github.com>
2023-11-04 19:00:42 +0400

    Add files via upload

---
+++ FileAPI.cpp
@@ -0,0 +1,59 @@
+#include "FileAPI.h"
+
+bool _declspec(dllexport) readFile(FILE f)
+{
+	HANDLE file = CreateFile((LPCWSTR)f.path, f.flag, f.shareMode, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if (file == INVALID_HANDLE_VALUE) {
+		std::cerr << "Couldn't open file:\n" << GetLastError();
+		return 0;
+		CloseHandle(file);
+	}
+
+	LARGE_INTEGER fSize;
+	if (!GetFileSizeEx(file, &fSize)) {
+		std::cerr << "Couldn't get file size:\n" << GetLastError();
+		return 0;
+		CloseHandle(file);
+	}
+
+	f.data.resize(fSize.QuadPart);
+
+	DWORD bytes = 0;
+	bool r = ReadFile(file, f.data.data(), fSize.QuadPart, &bytes, nullptr);
+
+	CloseHandle(file);
+
+	if (!r || bytes != fSize.QuadPart) {
+		return 0;
+	}
+	else {
+		return 1;
+	}
+}
+
+bool _declspec(dllexport) writeFile(FILE f)
+{
+	HANDLE file = CreateFile((LPCWSTR)f.path, f.flag, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if (file == INVALID_HANDLE_VALUE) {
+		std::cerr << "Couldn't open file\n" << GetLastError();
+		CloseHandle(file);
+		return 0;
+	}
+
+	if (!f.buffer || f.s == 0) {
+		std::cerr << "No file to write to\n";
+		CloseHandle(file);
+		return 0;
+	}
+
+	DWORD bytes = 0;
+	bool w = WriteFile(file, f.buffer, f.s, &bytes, NULL);
+
+	if (bytes != f.s) { w = 0; }
+
+	CloseHandle(file);
+
+	return w;
+}
--- FileAPI.h
+++ FileAPI.h
@@ -1,1 +1,23 @@
 #pragma once
+#include "framework.h"
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#define READ GENERIC_READ
+#define SHARE_READ FILE_SHARE_READ
+
+#define WRITE GENERIC_WRITE
+
+
+struct FILE {
+	const char* path;
+	std::vector<unsigned char> &data;
+	void* buffer;
+	size_t s;
+	int flag;
+	int shareMode;
+};
+bool _declspec(dllexport) readFile(FILE f);
+bool _declspec(dllexport) writeFile(FILE f);
